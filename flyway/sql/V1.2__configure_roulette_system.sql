efa05c7d1ada59994bc256d17bfbc11691e85825
João Paulo Alves Ferreira <iamferreirajp@gmail.com>
2017-07-03 18:09:43 -0300

    Configurando o sistema de recompensa da roleta

---
+++ flyway/sql/V1.2__configure_roulette_system.sql
@@ -0,0 +1,64 @@
+-- Configure the roulette items with the following pattern:
+-- 1. index is incremented and can`t be equal to other
+-- 2. level is the row of the roulette
+-- 3. item_id is the id of the item to appear in the roulette
+-- 4. amount is the number of the itens that is given
+-- 5. flag, 0 to items that allow the progression and 1 to hold the progression.
+
+-- Row 1 has 9 items, 2 has 8, 3 has 7, 4 has 6, 5 has 5, 6 has 4 has 7 has 3.
+
+-- Row 1
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('1', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('2', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('3', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('4', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('5', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('6', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('7', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('8', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('9', '1', '501', '10', '0');
+
+-- Row 2
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('10', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('11', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('12', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('13', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('14', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('15', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('16', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('17', '1', '501', '10', '0');
+
+-- Row 3
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('18', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('19', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('20', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('21', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('22', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('23', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('24', '1', '501', '10', '0');
+
+-- Row 4
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('25', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('26', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('27', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('28', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('29', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('30', '1', '501', '10', '0');
+
+-- Row 5
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('31', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('32', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('33', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('34', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('35', '1', '501', '10', '0');
+
+-- Row 6
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('36', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('37', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('38', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('39', '1', '501', '10', '0');
+
+-- Row 7
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('40', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('41', '1', '501', '10', '0');
+INSERT INTO `db_roulette` (`index`, `level`, `item_id`, `amount`, `flag`) VALUES ('42', '1', '501', '10', '0');
